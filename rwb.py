1b7e6f29899b472e0a4c885f7d3893d20daf1904
Joel Maher <joel.maher@gmail.com>
2013-10-11 15:29:31 -0400

    support for cli options, binary, logfile, profile

--- rwb.py
+++ rwb.py
@@ -2,12 +2,76 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import glob
 import unittest
+from optparse import OptionParser
+import os
+import sys
+
+class LiveMetricsOptions(OptionParser):
+    def __init__(self, **kwargs):
+        OptionParser.__init__(self, **kwargs)
+        self.add_option("-t", "--test-path",
+                        action="store", type="string", dest="testPath",
+                        default=None, help="path to the test manifest")
+
+        self.add_option("-b", "--binary",
+                        action="store", type="string", dest="binary",
+                        help="absolute path to application, overriding default")
+
+        self.add_option("--log-file",
+                        action="store", type="string", dest="logFile",
+                        metavar="FILE", default='livemetrics.log',
+                        help="file to which logging occurs")
+
+        self.add_option("-p", "--profile-path", action="store",
+                        type="string", dest="profilePath",
+                        default=None,
+                        help="path to the profile to use. "
+                             "If none specified, a temporary profile is created")
+
+        usage = """
+                Usage instructions for runlive.py.
+                %prog [options]
+                All arguments except --binary are optional.
+                """
+
+        self.set_usage(usage)
+
+    def verifyOptions(self, options):
+        """ verify correct options and cleanup paths """
+        if not options.binary:
+            if not os.path.exists(options.binary):
+                print "error: --binary must specify the path to a browser"
+                return None
+        return options
 
 def main():
+    parser = LiveMetricsOptions()
+    options, args = parser.parse_args()
+    options = parser.verifyOptions(options)
+    if options == None:
+        return 2
+
     test_file_strings = glob.glob('test_*.py')
-    module_strings = [str[0:len(str)-3] for str in test_file_strings]
-    suites = [unittest.defaultTestLoader.loadTestsFromName(str) for str
-              in module_strings]
+    module_strings = [str[5:len(str)-3] for str in test_file_strings]
+    testsToRun = module_strings
+    if options.testPath:
+        testsToRun = []
+        for test in options.testPath:
+            if test not in module_strings:
+                print "error: test %s not found in these tests %s" % (test, module_strings)
+                return 2
+            testsToRun.append(test)
+
+    suites = [unittest.defaultTestLoader.loadTestsFromName('test_' + str) for str
+              in testsToRun]
+
+    command_line_args = options
+
+    #HACK: the test case in setUp() will parse sys.argv[1:], so we define our args
+    del sys.argv[1:]
+    sys.argv.append(options.binary)
+    sys.argv.append(options.logFile)
+    sys.argv.append(options.profilePath)
 
     testSuite = unittest.TestSuite(suites)
     text_runner = unittest.TextTestRunner().run(testSuite)
--- testBenchmark.py
+++ testBenchmark.py
@@ -1,26 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import unittest
+import sys
+import os
 
 class tBenchmark(unittest.TestCase):
 
     def setUp(self):
-        # setup profile
-        # use prefs file
-        # support addons
-        # add custom addon for record/reset/stop/collect/gc as a sample interface
-        self.profile = webdriver.FirefoxProfile()
-        prefs = {'mfl.logfile': '/home/jmaher/rwb/rwb.log',
+        self.binary = sys.argv[1]
+        self.logFile = sys.argv[2]
+        self.profilePath = sys.argv[3]
+
+        if self.profilePath and os.path.exists(self.profilePath):
+            self.profile = webdriver.FirefoxProfile(self.profilePath)
+        else:
+            self.profile = webdriver.FirefoxProfile()
+
+        # TODO: use prefs file
+        prefs = {'mfl.logfile': self.logFile,
                  'browser.dom.window.dump.enabled': True}
         for pref in prefs:
             self.profile.set_preference(pref, prefs[pref])
 
-        addons = ['collector/collector.xpi']
+        addons = ['collector.xpi']
         for addon in addons:
             self.profile.add_extension(extension=addon)
 
-        # firefox app definition
-        self.driver = webdriver.Firefox(self.profile)
+        if self.binary and os.path.exists(self.binary):
+            binary = FirefoxBinary(self.binary)
+            self.driver = webdriver.Firefox(firefox_profile=self.profile, firefox_binary=binary)
+        else:
+            self.driver = webdriver.Firefox(firefox_profile=self.profile)
 
     def tearDown(self):
         self.driver.quit()
